ac8d6c69b9f09620c2968cfe5378674c1dbe674f
xyf <1498542329@qq.com>
2025-01-06 15:38:57 +0800

    自动创建数据库文件夹

--- src/main/java/org/pocket/web/server/DatabaseInitializer.java
+++ src/main/java/org/pocket/web/server/DatabaseInitializer.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,8 +16,16 @@ public class DatabaseInitializer {
     @Value("${database.url}")
     private String databaseUrl;
 
+    @Value("${database.dir}")
+    private String databaseDir;
+
     @PostConstruct
     public void initDatabase() throws SQLException {
+        File directory = new File(databaseDir);
+        if (!directory.exists()) {
+            boolean isCreated = directory.mkdirs();
+
+        }
         Connection connection = DriverManager.getConnection(databaseUrl);
         System.out.println("成功建立数据库连接" + databaseUrl);
     }
