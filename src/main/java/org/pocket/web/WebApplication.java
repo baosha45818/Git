e0faba370805adb83b7c07a3fbb98e2783a3da78
lyh <liaoyinghai@yymt.com>
2025-01-06 10:05:11 +0800

    submit

---
+++ src/main/java/org/pocket/web/WebApplication.java
@@ -0,0 +1,13 @@
+package org.pocket.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages="org.pocket.web.server")
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/pocket/web/server/Rest.java
@@ -0,0 +1,23 @@
+package org.pocket.web.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lyh
+ * @version 1.0.0
+ * @date 2025-01-06 上午9:53:37
+ */
+@Controller
+@RestController
+public class Rest {
+
+    @GetMapping("/info")
+    public String info(String infoDto) {
+        if(infoDto == null || infoDto.isEmpty()){
+            infoDto = "{\"info\":\"\"}";
+        }
+        return infoDto;
+    }
+
+}
